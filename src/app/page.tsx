8149e50f3d4d2e4e6acf672a18b95848d820a8c1
Ivan B <sensenabi@gmail.com>
2024-02-14 11:04:28 -0500

    update home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        SSR Examples (us-east-1)
+        SSR Examples (sa-east-1)
       </div>
       <p>Cache Tests Far (150ms simulated latency)</p>
       <div className="flex flex-col px-4">
